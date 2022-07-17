f1e46aa9f83b50b3c6eee8e38284b905c3fb8224
Danniel Cristie <dany14012@gmail.com>
2022-07-17 00:45:22 -0300

    Entedendo hash e implementando com crypto-js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-    
+const sha256 = require('crypto-js/sha256');
+
+/* HASH */
+//conteudo do hash
+const transaction = {
+    from: "a",
+    to: "b",
+    amount: "1btc"
+};
+
+const hash = sha256(JSON.stringify(transaction)).toString();
+
+console.log("hash com a trasação original: ", hash);
+
+//alterando o a trasação original
+transaction.from = "c";
+
+console.log("hash com a trasação alterada: ",
+    sha256(
+        JSON.stringify(
+            transaction)
+    ).toString()
+)
